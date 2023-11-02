f9dc7a8f81d9284d17d270ed617761e8575ddcc0
lgallogomez <l.gallogomez@gmail.com>
2023-11-02 14:35:19 -0500

    updated 3  file

--- models/base_model.py
+++ models/base_model.py
@@ -16,9 +16,6 @@ class BaseModel:
     This class will be the base for a series of subclasses
     """
     def __init__(self, *args, **kwargs):
-        #if kwargs:
-         #   self.create_from_dic(**kwargs)
-        #else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
