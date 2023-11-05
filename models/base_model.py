306623945d9edaa0dbf89dcfcb74009a477dc0f2
lgallogomez <l.gallogomez@gmail.com>
2023-11-05 17:52:35 -0500

    updated 4th file

--- models/base_model.py
+++ models/base_model.py
@@ -5,10 +5,17 @@ import uuid
 
 class BaseModel:
 
-    def __init__(self):
-        self.id = str(uuid.uuid4()) 
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4()) 
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return(f"[{__class__.__name__}] ({self.id}) {self.__dict__}")
@@ -25,3 +32,19 @@ class BaseModel:
         dicti["created_at"] = self.created_at.isoformat()
         dicti["updated_at"] = self.updated_at.isoformat()  
         return dicti
+
+
+my_model = BaseModel()
+my_model.name = "My_First_Model"
+my_model.my_number = 89
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("----")
+new_model = BaseModel(**my_model_json)
+print(new_model)
+print(new_model.id)
+print(new_model)
+print(type(new_model.created_at))
+
+print("--")
+print(my_model is new_model)
