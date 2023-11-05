8ea54c7f87a0d53bce2cc5e2d51e3ed84f818b09
lgallogomez <l.gallogomez@gmail.com>
2023-11-04 21:08:59 -0500

    updated 3th file

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,7 @@ import uuid
 class Base_Model:
 
     def __init__(self):
-        self.id = uuid.uuid4() 
+        self.id = str(uuid.uuid4()) 
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
@@ -17,21 +17,10 @@ class Base_Model:
         self.updated_at = self.updated_at.now()
 
     def to_dict(self):
-        obj_dict = {
-            "__class__": self.__dict__,
-            "id": self.id,
-            "created_at": self.created_at.isoformat(),
-            "updated_at": self.updated_at.isoformat(),
-        }
-        return(obj_dict)
-
-mi_clase = Base_Model()
-mi_clase.name = "My first model"
-mi_clase.mynumber = 89
-
-mi_clase.save()
-print(mi_clase)
-mi_clase.save()
-print("-----")
-print(mi_clase)
-
+        dicti = {}
+        for key, value in vars(self).items():    
+           dicti[key] = value
+        dicti["__class__"]: __class__.__name__
+        dicti["created_at"]: self.created_at.isoformat()
+        dicti["updated_at"]: self.updated_at.isoformat()  
+        return dicti
