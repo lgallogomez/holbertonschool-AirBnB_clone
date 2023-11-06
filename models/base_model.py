ab1fddf9f0360d6e3c2d7f4a969ac87c6a5a8c61
lgallogomez <l.gallogomez@gmail.com>
2023-11-06 12:08:59 -0500

    updated 4th file

--- models/base_model.py
+++ models/base_model.py
@@ -3,15 +3,13 @@
 from datetime import datetime
 import uuid
 
+
+iso_formated_time = '%Y-%m-%dT%H:%M:%S.%f'
 class BaseModel:
 
     def __init__(self, **kwargs):
         if kwargs:
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    setattr(self, key, value)
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.from_kwargs(**kwargs)
         else:
             self.id = str(uuid.uuid4()) 
             self.created_at = datetime.now()
@@ -32,3 +30,10 @@ class BaseModel:
         dicti["created_at"] = self.created_at.isoformat()
         dicti["updated_at"] = self.updated_at.isoformat()  
         return dicti
+    
+    def from_kwargs(self, **kwargs):
+        for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
+        self.created_at = datetime.strptime(kwargs['created_at'], iso_formated_time)
+        self.updated_at = datetime.strptime(kwargs['updated_at'], iso_formated_time)
