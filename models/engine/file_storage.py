c48b3f56ba611df41d5846ce6e67d76e1f0eb69f
lgallogomez <l.gallogomez@gmail.com>
2023-11-10 14:44:59 -0500

    created files

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/python3
 
 import json
@@ -42,38 +41,3 @@ class FileStorage():
                 self.__objects = json.loads(read_str)
         except Exception as e:
             pass
-
-=======
-class FileStorage():
-    """
-    __file_path: stores the jason file
-    __objects: empty dict to store a BaseModel object. 
-    keys with following format: <class name>.id ex:BaseModel.12121212
-    where 12121212 is the id and BaseModel is the class
-    """
-    __file_path = "file.json"
-    __objects = dict()
-
-    def __init__(self):
-        pass
-
-    def all(self):
-        return (self.__objects)
-
-    def new(self, obj):
-        print(self.__objects)
-        key = "{0}.{1}".format(obj.__class__.__name__, obj.id)
-        print(key)
-        print(obj)
-        self.__objects[key] = obj
-        print(self.__objects)
-  
-    def save(self):
-        pass
-
-    def reload(self):
-        pass
-
-file = FileStorage()
-file.new()
->>>>>>> 240ed285581b939eccaded472388ed8c7d347a5e
