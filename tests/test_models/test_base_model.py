d22b2b56ff68459aa81f839c4c63de99833667a1
lgallogomez <l.gallogomez@gmail.com>
2023-11-15 13:48:55 -0500

    updated test, added test_to_dict test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -6,14 +6,28 @@ from datetime import datetime
 
 
 class Test_Base_Model_outputs(unittest.TestCase):
+    '''
+    tests method in BaseModel class
+    '''
+    
     def test_save(self):
+        '''
+        tests the save method in BaseModel class, where updated_at attribute
+        updates itself everytime the object/instance is saved
+        '''
         new_object = BaseModel()
         new_object.save()
         first_save = new_object.updated_at
         new_object.save()
         second_save = new_object.updated_at
         self.assertNotEqual(first_save, second_save)
-    
    
+    def test_to_dict(self):
+        new_object = BaseModel()
+        new_dict = new_object.to_dict()
+        self.assertIs(type(new_dict), dict)
+        self.assertIsNot(new_dict, new_object)
+
+
 if __name__ == "__main__":
     unittest.main()
