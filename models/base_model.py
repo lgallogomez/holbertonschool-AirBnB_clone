7c6bd5dda1174fed1294c653de64979d26c1dd17
lgallogomez <l.gallogomez@gmail.com>
2023-11-15 16:18:35 -0500

    updated base_model

--- models/base_model.py
+++ models/base_model.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 
 
-'''
+"""
 this module defines a class that defines methods and
 attributes for other classes
-'''
+"""
 
 
 from datetime import datetime
@@ -17,40 +17,51 @@ iso_formated_time = '%Y-%m-%dT%H:%M:%S.%f'
 class BaseModel:
 
 
-    '''
+    """
     class defines common methods for other classes
-    '''
+    """
     def __init__(self, *args, **kwargs):
-        '''
+        """
         if intance args receives a dict, makes dict an obj
         if not, creates object & stores in dict of objects using storage.new
-        '''
+        """
         if kwargs:
             self.from_kwargs(**kwargs)
         else:
-            self.id = str(uuid.uuid4()) 
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
         
     def __str__(self):
+        """
+        defines and returns the format to print
+        """
         return(f"[{__class__.__name__}] ({self.id}) {self.__dict__}")
 
     def save(self):
+        """
+        updates object attribute updated_at
+        """
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
+        """
+        takes an basemodel object and transforms into dictionary
+        """
         dicti = {}
-        for key, value in vars(self).items():    
+        for key, value in vars(self).items():
            dicti[key] = value
-
         dicti["__class__"] = self.__class__.__name__
         dicti["created_at"] = self.created_at.isoformat()
-        dicti["updated_at"] = self.updated_at.isoformat()  
+        dicti["updated_at"] = self.updated_at.isoformat() 
         return dicti
 
     def from_kwargs(self, **kwargs):
+        """
+        creates an instance from a dictionary
+        """
         for key, value in kwargs.items():
                 if key != '__class__':
                     setattr(self, key, value)
