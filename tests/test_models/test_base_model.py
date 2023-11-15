a2b6606e61b756c91dcab40aeb34c421671b7068
lgallogomez <l.gallogomez@gmail.com>
2023-11-15 12:40:37 -0500

    updated test, added variables to created and updated

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -8,11 +8,12 @@ from datetime import datetime
 class Test_Base_Model_outputs(unittest.TestCase):
     def test_save(self):
         new_object = BaseModel()
-        before_save = new_object.updated_at
         new_object.save()
-        after_save = new_object.updated_at
-        self.assertNotEqual(before_save, after_save)
-
+        first_save = new_object.updated_at
+        new_object.save()
+        second_save = new_object.updated_at
+        self.assertNotEqual(first_save, second_save)
+    
    
 if __name__ == "__main__":
     unittest.main()
