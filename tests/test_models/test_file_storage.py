d8ec653e713b3b7f7bb2d0f6b25e25e542984441
lgallogomez <l.gallogomez@gmail.com>
2023-11-15 22:49:20 -0500

    created test at filestorage in method all

--- tests/test_models/test_file_storage.py
+++ tests/test_models/test_file_storage.py
@@ -9,16 +9,17 @@ import unittest
 
 from models.engine import file_storage
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
 
 class test_file_storage(unittest.TestCase):
     """
     this class test file storage methods
     """
 
-    def test_file_path(self):
+    def test_all(self):
         """
-        tests if file path is created after creating
-        a file storage object
+        tests if method returns a dict of objects
         """
+        new_object = BaseModel()
         new_storage = FileStorage()
-        self.assertNotEqual(new_storage.__file_path, None)
+        self.assertNotEqual(new_storage.all, {})
