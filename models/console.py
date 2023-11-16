0dd9b52f997b192b2e1e31fbbb965e19e1a1285e
lgallogomez <l.gallogomez@gmail.com>
2023-11-16 14:52:53 -0500

    created console file with methods

--- models/console.py
+++ models/console.py
@@ -18,6 +18,9 @@ class HBNBCommand(cmd.Cmd):
     
     def do_quit(self, arg):
         return True
+    
+    def do_EOF(self):
+        return True
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
