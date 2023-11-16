b37f6edfe273e3a6a60580a2c770588b301fe4f9
lgallogomez <l.gallogomez@gmail.com>
2023-11-16 17:08:48 -0500

    updated console file with do_create method

--- console.py
+++ console.py
@@ -5,12 +5,20 @@ This program contains the entry point of the command interpreter
 """
 
 import cmd, sys
+from models.base_model import BaseModel
+from models import engine
 
 class HBNBCommand(cmd.Cmd):
     """
     class that inherits all cmd's methods
     """
     prompt = "(hbnb)"
+
+    def do_create(self, model):
+        if model:
+            model = BaseModel()
+            model.save()
+            print(model.id)
     
     def emptyline(self):
         pass
@@ -20,6 +28,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_EOF(self):
         True
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
