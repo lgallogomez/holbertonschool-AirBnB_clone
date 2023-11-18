33a4ef9bb26c0fa342956f14b5e8fbc558a07f65
lgallogomez <l.gallogomez@gmail.com>
2023-11-18 12:44:11 -0500

    updated destoy method saving after destoying. updated objects after applying save method in storage object

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
 
         objects = storage.all()
         objects.pop(object_key)
-        objects.storage.save()
+        objects = storage.save()
 
     def do_all(self, line):
         if not line:
