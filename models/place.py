e70092608c158560a962ba8abd2f179f96696ee3
lgallogomez <l.gallogomez@gmail.com>
2023-12-01 15:03:11 -0500

    added place_id

--- models/place.py
+++ models/place.py
@@ -16,7 +16,7 @@ class Place(BaseModel):
     description = str()
     number_rooms = int(0)
     number_bathrooms = int(0)
-    max_guest = float(0.0)
+    max_guest = int(0)
     price_by_night = int(0)
     latitude = float(0.0)
     longitude = float(0.0)
