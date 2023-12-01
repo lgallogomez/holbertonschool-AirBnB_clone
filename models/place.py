eb56c1882e159de24fca022dcd4381d66374bdd8
lgallogomez <l.gallogomez@gmail.com>
2023-12-01 14:35:36 -0500

    created classes Place & Review

---
+++ models/place.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+module initializes a state class that inherits attributes from BaseModel
+"""
+
+from models.base_model import BaseModel
+
+class Place(BaseModel):
+    """
+    class creates an Place
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.city_id = str()
+        self.user_id = str()
+        self.name = str()
+        self.description = str()
+        self.number_rooms = 0
+        self.number_bathrooms = 0
+        self.max_guest = float(0.0)
+        self.latitude = float(0.0)
+        self.longitude = float(0.0)
+        self.amenity_ids = list()
+
---
+++ models/review.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+module creates a class Review
+"""
+
+from models.base_model import BaseModel
+
+class Review(BaseModel):
+    """
+    class inherits methods & attributes of basemodel & initializes it's own
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.place = str()
+        self.user_id = str()
+        self.text = str()
