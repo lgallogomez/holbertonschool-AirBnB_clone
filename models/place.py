9edc32f0005689a57892b5616de63816b4780a85
lgallogomez <l.gallogomez@gmail.com>
2023-12-01 14:59:42 -0500

    added price_by_night class attribute

--- models/place.py
+++ models/place.py
@@ -14,9 +14,10 @@ class Place(BaseModel):
     user_id = str()
     name = str()
     description = str()
-    number_rooms = 0
-    number_bathrooms = 0
+    number_rooms = int(0)
+    number_bathrooms = int(0)
     max_guest = float(0.0)
+    price_by_night = int(0)
     latitude = float(0.0)
     longitude = float(0.0)
     amenity_ids = list()
