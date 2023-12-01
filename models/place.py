2a619613ed4b3a0335485b1b30d8a28fdbbca560
lgallogomez <l.gallogomez@gmail.com>
2023-12-01 14:57:47 -0500

    self erased from public class attributes

--- models/place.py
+++ models/place.py
@@ -10,16 +10,16 @@ class Place(BaseModel):
     """
     class creates an Place
     """
-    self.city_id = str()
-    self.user_id = str()
-    self.name = str()
-    self.description = str()
-    self.number_rooms = 0
-    self.number_bathrooms = 0
-    self.max_guest = float(0.0)
-    self.latitude = float(0.0)
-    self.longitude = float(0.0)
-    self.amenity_ids = list()
+    city_id = str()
+    user_id = str()
+    name = str()
+    description = str()
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = float(0.0)
+    latitude = float(0.0)
+    longitude = float(0.0)
+    amenity_ids = list()
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
