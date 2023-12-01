1135dddab517901745064e8fb0a093ca6a90226d
lgallogomez <l.gallogomez@gmail.com>
2023-12-01 14:44:55 -0500

    names now have empty string

--- models/state.py
+++ models/state.py
@@ -5,11 +5,11 @@ module initializes a state class that inherits attributes from BaseModel
 """
 
 from models.base_model import BaseModel
-
 class State(BaseModel):
     """
     class creates an state
     """
+    
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.name = ""
+        self.name = str()
