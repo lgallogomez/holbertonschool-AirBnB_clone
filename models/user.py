972b8b416b09fb6f10b0c3a76e26c1ce235e8f8e
lgallogomez <l.gallogomez@gmail.com>
2023-12-04 16:30:45 -0500

    added attributes to instance user

--- models/user.py
+++ models/user.py
@@ -17,3 +17,7 @@ class User(BaseModel):
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.email = str()
+        self.password = str()
+        self.first_name = str()
+        self.last_name = str()
